0ac3a06fb035c2ff9f8ec742e53ee862aab71519
Clintonkipchirchir <clintonkipkemoi2@gmail.com>
2025-01-19 21:07:17 +0300

    update file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,10 +9,10 @@ class BankAccount:
         sufficient_funds = self.account_balance - amount
         if sufficient_funds > 0 or sufficient_funds == 0:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount}")
+            return True
         else:
-            print("Insufficient funds")
+            return False
     
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
