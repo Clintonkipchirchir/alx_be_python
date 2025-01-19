6dd9eeee656a4a87d9051b32ebcdc058f3886f1e
Clintonkipchirchir <clintonkipkemoi2@gmail.com>
2025-01-19 23:22:52 +0300

    test calc file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,7 +14,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-2, 2), 0)
         self.assertEqual(self.calc.add(10, 2), 12)
     
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(4, 3), 1)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
