32023b5b52bc927faed5755dcb426e9f27e4912e
Clintonkipchirchir <clintonkipkemoi2@gmail.com>
2025-01-19 23:20:33 +0300

    test calc file

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,2 +1,27 @@
 class Book:
+    def __init__(self, title, authour):
+        self.title = title
+        self.author = authour
+        self.___is_checked_out = True
+
+class Library(Book):
+    def __init__(self, ):
+        super().__init__(title, authour)
+        self.__books = []
     
+    def add_book(self):
+        self.__books.append(Book)
+    
+    def check_out_book(self, title):
+        self.__books.remove(title)
+
+    def return_book(self, title):
+        pass 
+
+    def list_available_books(self):
+        return self.__books
+    
+
+library = Library
+library.add_book(Book("Brave New World", "Aldous Huxley"))
+library.add_book(Book("1984", "George Orwell"))
---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,37 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-2, 2), 0)
+        self.assertEqual(self.calc.add(10, 2), 12)
+    
+    def test_subtract(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(4, 3), 1)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(21, 2), 19)
+        self.assertEqual(self.calc.subtract(10, 2), 8)
+    
+    def test_divide(self):
+        """Test the divide method."""
+        self.assertEqual(self.calc.divide(-14, 2), -7)
+        self.assertEqual(self.calc.divide(9, 4), 2.25)
+        self.assertEqual(self.calc.divide(0, 2), 0)
+        self.assertEqual(self.calc.divide(10, 0), 0)
+    
+    def test_multiply(self):
+        """Test the multiply method."""
+        self.assertEqual(self.calc.multiply(0, 12),0 )
+        self.assertEqual(self.calc.multiply(-3, -4), 12)
+        self.assertEqual(self.calc.multiply(11, 2), 22)
+        self.assertEqual(self.calc.multiply(10, 2), 20)
+    
