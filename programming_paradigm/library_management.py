30976ae66eabb7aa970f4f97a7b954ffaffc87b4
Clintonkipchirchir <clintonkipkemoi2@gmail.com>
2025-01-30 01:06:46 +0300

    semi complete

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,27 +2,49 @@ class Book:
     def __init__(self, title, authour):
         self.title = title
         self.author = authour
-        self.___is_checked_out = True
-        self._books = []
-    def return_book(self):
-        pass
-
-class Library(Book):
+        self._is_checked_out = True
+    
+class Library:
    
     def __init__(self):
         self._books = []
     
-    def add_book(self):
-        self._books = []
-        self._books.append(Book)
+    def add_book(self, book):
+        self._books.append(book)
     
     def check_out_book(self, title):
-        self._books = []
-        self._books.remove(title)
-
+        for book in self._books:
+            if book.title == title:
+                self._books.remove(book)
+                return True
+        return False
     def return_book(self, title):
-        self._books = []
-
+        for book in self._books:
+            if book.title == title:
+                self._books.append(book)
+                return True
+        return False
+        
     def list_available_books(self):
-        self._books = []
-    
+        for book in self._books:
+            print(book.title + " by " + book.author)
+
+
+  # Setup a small library
+library = Library()
+library.add_book(Book("Brave New World", "Aldous Huxley"))
+library.add_book(Book("1984", "George Orwell"))
+
+# Initial list of available books
+print("Available books after setup:")
+library.list_available_books()
+
+# Simulate checking out a book
+library.check_out_book("1984")
+print("\nAvailable books after checking out '1984':")
+library.list_available_books()
+
+# Simulate returning a book
+library.return_book("1984")
+print("\nAvailable books after returning '1984':")
+library.list_available_books()
