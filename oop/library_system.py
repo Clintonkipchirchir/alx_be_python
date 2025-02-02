702d2bcd1f215ae0a308654d25ac5b46f819c399
Clintonkipchirchir <clintonkipkemoi2@gmail.com>
2025-02-03 01:00:30 +0300

    lib sytem

---
+++ oop/library_system.py
@@ -0,0 +1,39 @@
+class Book:
+
+    books = []
+    def __init__(self, title :str, authour:str):
+        self.title = title
+        self.author = authour
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}{self.title} by {self.author}"
+
+class EBook(Book):
+
+    def __init__(self, title :str, authour:str, file_size:float):
+        super().__init__(title, authour)
+        self.file_size = file_size
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}{self.title} by {self.author}, File Size: {self.file_size}KB"
+
+class PrintBook(Book):
+
+    def __init__(self, title :str, authour:str, page_count:float):
+        super().__init__(title, authour)
+        self.page_count = page_count
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}{self.title} by {self.author}, Page Count: {self.page_count}"
+
+class Library:
+   
+    def __init__(self):
+        self._books = []
+    
+    def add_book(self, book):
+        self._books.append(book)
+    
+    def list_books(self):
+        for book in self._books:
+            print(book)
